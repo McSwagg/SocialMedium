ab288556bc9a08aba5ab0f777bbbb2a11c213f3d
Nathan Wawro <nathanwawro@gmail.com>
2021-09-21 07:17:50 -0600

    Initial Commit

---
+++ SocialMedium/SocialMediumApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediumApp.swift
+//  SocialMedium
+//
+//  Created by Nathan Wawro on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
